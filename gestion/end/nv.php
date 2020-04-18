80c5dce30b2132c1fb4b2e658e5994a3a78ca243
Victorien Marette <VictorienMarette.Pro@gmail.com>
2020-04-18 15:26:32 +0200

    nv1

--- gestion/end/nv.php
+++ gestion/end/nv.php
@@ -1,6 +1,12 @@
 <?php
 if (isset($_POST['poug'])) {
 	include_once("../../includes/end/dph.php");
+	if (mysqli_connect_errno($conn)) {
+   
+		echo "Failed to connect to MySQL: " . mysqli_connect_error($conn);
+	}else{
+		echo "Pas de pb PUUUUUUUTTTTTAIIIIIIIINNNNNNN";
+	}
 	if ($_POST['poug'] == 'p') {
 		$sql = "INSERT INTO partenaires (p_lien, g_indic) VALUES ('nouveau', '0');";
 		$result = mysqli_query($conn, $sql);
